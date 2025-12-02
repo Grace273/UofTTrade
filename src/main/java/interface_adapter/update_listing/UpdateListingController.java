99fd97c0930f44030b6654850859ee604cbce0c9
tinagao <tinagao426@gmail.com>
2025-12-01 19:32:49 -0500

    correct most of checkstyle problems related to delete listing

--- src/main/java/interface_adapter/update_listing/UpdateListingController.java
+++ src/main/java/interface_adapter/update_listing/UpdateListingController.java
@@ -4,7 +4,6 @@ import entity.Listing;
 import entity.User;
 import use_case.update_listing.UpdateListingInputBoundary;
 import use_case.update_listing.UpdateListingInputData;
-import use_case.update_listing.UpdateListingOutputData;
 
 public class UpdateListingController {
     private final UpdateListingInputBoundary updateListingUseCaseInteractor;
@@ -13,6 +12,19 @@ public class UpdateListingController {
         this.updateListingUseCaseInteractor = updateListingUseCaseInteractor;
     }
 
+    /**
+     * Executes the update listing use case with the given parameters.
+     *
+     * <p>
+     * This method wraps the provided deletion flag, user, and listing into an
+     * {@link UpdateListingInputData} object and delegates execution to the
+     * {@code updateListingUseCaseInteractor}.
+     * </p>
+     *
+     * @param isDelete {@code true} if the listing should be deleted; {@code false} otherwise
+     * @param user     the user who owns or is associated with the listing
+     * @param listing  the listing to be updated or deleted
+     */
     public void execute(boolean isDelete, User user, Listing listing) {
         final UpdateListingInputData updateListingInputData = new UpdateListingInputData(
                 isDelete, user, listing);
--- src/main/java/use_case/update_listing/UpdateListingUserDataAccessInterface.java
+++ src/main/java/use_case/update_listing/UpdateListingUserDataAccessInterface.java
@@ -1,5 +1,15 @@
 package use_case.update_listing;
 
 public interface UpdateListingUserDataAccessInterface {
+    /**
+     * Updates the persistent record of the listing with the given ID.
+     *
+     * <p>
+     * Implementations of this method delete listing associated with given ID and
+     * syncing in-memory changes to the underlying data store.
+     * </p>
+     *
+     * @param listingId the unique identifier of the listing to update in storage
+     */
     void updateListing(int listingId);
 }
