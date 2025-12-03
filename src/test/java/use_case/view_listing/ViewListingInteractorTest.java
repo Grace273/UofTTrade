28afd82ca8a0f775899abb70c19dc05ebc5f369e
TutorialBot77 <vboxacct@hotmail.com>
2025-12-02 23:39:05 -0500

    Updated tests to prevent API use

--- src/test/java/use_case/view_listing/ViewListingInteractorTest.java
+++ src/test/java/use_case/view_listing/ViewListingInteractorTest.java
@@ -5,29 +5,48 @@ import entity.User;
 
 import data_access.CreateListingDataAccessObject;
 import interface_adapter.view_listing.ViewListingController;
-import org.junit.jupiter.api.BeforeEach;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import use_case.login.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ViewListingInteractorTest {
 
-    // Added time between tests to avoid failing due to api limits
-    @BeforeEach
-    void delayBetweenTests() throws InterruptedException {
-        Thread.sleep(10000);
-    }
-
 
     @Test
     public void viewListingSuccessTest() throws IOException {
         ViewListingInputData inputData = new ViewListingInputData("Garbage Can", "TutorialBot77");
-        UserDataAccessObject userRepository = new UserDataAccessObject();
-        CreateListingDataAccessObject CreateListingDataAccessObject = new CreateListingDataAccessObject();
+        UserDataAccessObject userRepository = new UserDataAccessObject() {
+            @Override
+            public User getUser(String userIdentifier) {
+                return new User(userIdentifier, userIdentifier + "45", userIdentifier.toLowerCase()
+                        + "@gmail.com");
+            }
+        };
+        CreateListingDataAccessObject CreateListingDataAccessObject = new CreateListingDataAccessObject() {
+            @Override
+            public JSONObject getSpecificListingInfo(String listingName, String listingOwner) {
+                final JSONObject listingInfo = new JSONObject();
+                final List<String> categoryList = new ArrayList<String>();
+                categoryList.add("Category1");
+                categoryList.add("Category2");
+                listingInfo.put("Name", listingName);
+                listingInfo.put("Owner", listingOwner);
+                listingInfo.put("Description", "The Garbage Can");
+                listingInfo.put("Categories", categoryList);
+                listingInfo.put("ListingID", 1);
+                return listingInfo;
+            }
+        };
         User user = userRepository.getUser("TutorialBot77");
+        final List<String> categoryList = new ArrayList<String>();
+        categoryList.add("Category1");
+        categoryList.add("Category2");
 
         ViewListingOutputBoundary successPresenter = new ViewListingOutputBoundary() {
 
@@ -37,6 +56,7 @@ public class ViewListingInteractorTest {
                 assertEquals("TutorialBot77", data.getListingOwner());
                 assertEquals(user.get_email(), data.getListingOwnerEamil());
                 assertEquals("The Garbage Can", data.getListingDescription());
+                assertEquals(categoryList, data.getListingCategories());
             }
 
             @Override
@@ -60,7 +80,12 @@ public class ViewListingInteractorTest {
     @Test
     public void viewListingFailListingDoesntExist() throws IOException {
         UserDataAccessObject userRepository = new UserDataAccessObject();
-        CreateListingDataAccessObject CreateListingDataAccessObject = new CreateListingDataAccessObject();
+        CreateListingDataAccessObject CreateListingDataAccessObject = new CreateListingDataAccessObject() {
+            @Override
+            public JSONObject getSpecificListingInfo(String listingName, String listingOwner) {
+                return null;
+            }
+        };
         ViewListingInputData inputData = new ViewListingInputData("Mouse and Keyboard", "TutorialBot77");
 
         ViewListingOutputBoundary successPresenter = new ViewListingOutputBoundary() {
@@ -90,8 +115,28 @@ public class ViewListingInteractorTest {
     @Test
     public void testSwitchToPreviousView() throws IOException {
         ViewListingInputData inputData = new ViewListingInputData("Garbage Can", "TutorialBot77");
-        UserDataAccessObject userRepository = new UserDataAccessObject();
-        CreateListingDataAccessObject CreateListingDataAccessObject = new CreateListingDataAccessObject();
+        UserDataAccessObject userRepository = new UserDataAccessObject() {
+            @Override
+            public User getUser(String userIdentifier) {
+                return new User(userIdentifier, userIdentifier + "45", userIdentifier.toLowerCase()
+                        + "@gmail.com");
+            }
+        };
+        CreateListingDataAccessObject CreateListingDataAccessObject = new CreateListingDataAccessObject() {
+            @Override
+            public JSONObject getSpecificListingInfo(String listingName, String listingOwner) {
+                final JSONObject listingInfo = new JSONObject();
+                final List<String> categoryList = new ArrayList<String>();
+                categoryList.add("Category1");
+                categoryList.add("Category2");
+                listingInfo.put("Name", listingName);
+                listingInfo.put("Owner", listingOwner);
+                listingInfo.put("Description", "The Garbage Can");
+                listingInfo.put("Categories", categoryList);
+                listingInfo.put("ListingID", 1);
+                return listingInfo;
+            }
+        };
         User user = userRepository.getUser("TutorialBot77");
 
         ViewListingOutputBoundary successPresenter = new ViewListingOutputBoundary() {
